a3e959fd0b0fd65db61a0f096ff23a22ca414a66
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-07 12:48:27 +0200

    Update gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
 
-gulp.task('sass', function() {
+gulp.task('scss:build', function() {
   return gulp.src('./src/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('scss:watch', function () {
+  gulp.watch('./src/**/*.scss', ['scss']);
+});
+
+gulp.task('clean', function() {
+  return del(['dist/styles', 'dist/scripts', 'dist/images']);
 });

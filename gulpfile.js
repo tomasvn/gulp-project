b9e1268da6a90bbe30dc19948e51ac91f6f76f79
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-11 00:48:13 +0200

    Clean up gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,6 @@ var sass = require('gulp-sass');
 var del = require('del');
 var browserSync = require('browser-sync').create();
 
-gulp.task('scss:build', function() {
-  return gulp.src('./src/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('scss:watch', function () {
-  gulp.watch('./src/**/*.scss', ['scss']);
-});
-
 gulp.task('clean', function() {
   return del(['dist/styles', 'dist/scripts', 'dist/images']);
 });

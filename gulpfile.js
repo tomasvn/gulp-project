1a6ddac54db16b74657088bfd4745871ffc9b6d8
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-13 21:21:58 +0200

    Add js lint

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
-var browserSync = require('browser-sync').create(); //Create browser sync instance
+var browserSync = require('browser-sync').create(); // Create browser sync instance
 var del = require('del');
 
 var paths = {
@@ -22,36 +22,34 @@ var paths = {
 Developement Tasks
 */
 
-gulp.task('dev:styles', function() { //First argument is the name of the task, second argument callback function
-  return gulp.src(paths.stylesInput) //Look into this folder for any SCSS files
+gulp.task('dev:styles', function () { // First argument is the name of the task, second argument callback function
+  return gulp.src(paths.stylesInput) // Look into this folder for any SCSS files
     .pipe(sass())
-    .pipe(sass.sync().on('error', sass.logError)) //If SCSS syntax has any error output it to the CLI
+    .pipe(sass.sync().on('error', sass.logError)) // If SCSS syntax has any error output it to the CLI
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest(paths.stylesOutput)) //Compile SCSS files into one CSS file, output it here
+    .pipe(gulp.dest(paths.stylesOutput)) // Compile SCSS files into one CSS file, output it here
     .pipe(browserSync.stream())
 });
 
-gulp.task('watch', ['dev:styles'], function() {
-  
-  browserSync.init({ //Initialize browser sync
-    server: './src' //Input folder we want to serve to the browser
-  });
+gulp.task('watch', ['dev:styles'], function () {
+  browserSync.init({ // Initialize browser sync
+    server: './src' // Input folder we want to serve to the browser
+  })
 
-  gulp.watch(paths.stylesInput, ['dev:styles']); //Watch - it will run the styles task on file change
-  gulp.watch(paths.srcHtml).on('change', browserSync.reload); //Watch changes in HTML file and reload it browser
+  gulp.watch(paths.stylesInput, ['dev:styles']) // Watch - it will run the styles task on file change
+  gulp.watch(paths.srcHtml).on('change', browserSync.reload) // Watch changes in HTML file and reload it browser
 });
 
 /**
 Build Tasks
 */
 
-gulp.task('build:html', function() {
+gulp.task('build:html', function () {
   return gulp.src(paths.srcHtml)
     .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('build:styles', function() {
-  
+gulp.task('build:styles', function () {
   return gulp.src(paths.stylesInput)
     .pipe(sass())
     .pipe(autoprefixer({
@@ -68,10 +66,10 @@ gulp.task('build', ['build:html', 'build:styles']);
 Clean Tasks
 */
 
-gulp.task('clean:dev', function() {
-  return del([paths.stylesOutput]);
+gulp.task('clean:dev', function () {
+  return del([paths.stylesOutput])
 });
 
-gulp.task('clean', function() {
-  return del(['dist']); //Delete dist folder
+gulp.task('clean', function () {
+  return del(['dist']) // Delete dist folder
 });

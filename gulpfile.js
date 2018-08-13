96f66c8b54a834ca78fc0b650d174eb6e8d59a1f
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-13 21:29:03 +0200

    Fix lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ Created by: Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
 Github Repo: https://github.com/tomasvn/gulp-project.git
 **/
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cssnano = require('gulp-cssnano');
-var browserSync = require('browser-sync').create(); // Create browser sync instance
-var del = require('del');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
+var cssnano = require('gulp-cssnano')
+var browserSync = require('browser-sync').create() // Create browser sync instance
+var del = require('del')
 
 var paths = {
   stylesInput: './src/scss/*.scss',
@@ -29,7 +29,7 @@ gulp.task('dev:styles', function () { // First argument is the name of the task,
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(gulp.dest(paths.stylesOutput)) // Compile SCSS files into one CSS file, output it here
     .pipe(browserSync.stream())
-});
+})
 
 gulp.task('watch', ['dev:styles'], function () {
   browserSync.init({ // Initialize browser sync
@@ -38,7 +38,7 @@ gulp.task('watch', ['dev:styles'], function () {
 
   gulp.watch(paths.stylesInput, ['dev:styles']) // Watch - it will run the styles task on file change
   gulp.watch(paths.srcHtml).on('change', browserSync.reload) // Watch changes in HTML file and reload it browser
-});
+})
 
 /**
 Build Tasks
@@ -47,7 +47,7 @@ Build Tasks
 gulp.task('build:html', function () {
   return gulp.src(paths.srcHtml)
     .pipe(gulp.dest('./dist'))
-});
+})
 
 gulp.task('build:styles', function () {
   return gulp.src(paths.stylesInput)
@@ -58,9 +58,9 @@ gulp.task('build:styles', function () {
     }))
     .pipe(cssnano())
     .pipe(gulp.dest(paths.stylesDist))
-});
+})
 
-gulp.task('build', ['build:html', 'build:styles']);
+gulp.task('build', ['build:html', 'build:styles'])
 
 /**
 Clean Tasks
@@ -68,8 +68,8 @@ Clean Tasks
 
 gulp.task('clean:dev', function () {
   return del([paths.stylesOutput])
-});
+})
 
 gulp.task('clean', function () {
   return del(['dist']) // Delete dist folder
-});
+})

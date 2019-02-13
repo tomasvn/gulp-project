0e6af11b48f115105915db2a8a403f49da4b6d74
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2019-02-13 21:40:32 +0100

    Add nightwatch for e2e testing

--- e2e-tests/index.test.js
+++ e2e-tests/index.test.js
@@ -1,9 +1,9 @@
 module.exports = {
   'Index test' : function (browser) {
     browser
-      .url('http://localhost:3000/src/index.html')
+      .url('http://192.168.0.227:3000')
       .waitForElementVisible('body', 1000)
-      .assert.containsText('h1', 'Hello Nightwatch Test')
+      .assert.containsText('h1', 'Hello There')
       .end();
   }
 };
